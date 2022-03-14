ccb108cf27766d982127d5687dc0dbcbc1ad0cbf
Rainer Beth Andrew <100107630+rainerba@users.noreply.github.com>
2022-03-14 10:45:49 +0700

    Update test pickle.py

--- Minggu 1/Hari 5/test pickle.py
+++ Minggu 1/Hari 5/test pickle.py
@@ -10,9 +10,13 @@ class Sheep(Animal):
         Animal.__init__(self, 4, color)
 
 mary = Sheep("white")
+joni = Sheep("black")
 
 print (str.format("My sheep mary is {0} and has {1} paws", mary.color, mary.number_of_paws))
 my_pickled_mary = pickle.dumps(mary)
 
 print ("Would you like to see her pickled? Here she is!")
 print (my_pickled_mary)
+
+unpickled_mary = pickle.loads(my_pickled_mary)
+print(str.format("Mary is {0} and has {1} paws", unpickled_mary.color, unpickled_mary.number_of_paws))
